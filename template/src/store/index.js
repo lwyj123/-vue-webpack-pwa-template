856ddea8776e197fe2c90bdf075564c938b4e817
lwyj123 <liang.pearce@gmail.com>
2018-08-22 14:40:40 +0800

    fix: 修复workbox问题以及完成demo

--- template/src/store/index.js
+++ template/src/store/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import user from './modules/user'
+// import user from './modules/user'
 import getters from './getters'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
-    user
+    // user
   },
   getters
   // plugins: [plugin]
